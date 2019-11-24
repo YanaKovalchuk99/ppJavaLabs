9efa64d53f063b2bf573cdd37ebf3b50d77c32d6
yana <koval_yana99@mail.ru>
2019-11-24 20:44:08 +0300

    lab1

---
+++ lab1/src/main/java/ru/spbstu/telematics/java/MV.java
@@ -0,0 +1,42 @@
+package ru.spbstu.telematics.java;
+import java.io.File;
+import java.io.IOError;
+
+public class MV {
+
+	public static void main(String[] args)
+    {
+		if(args.length == 0)
+			System.out.println("Ошибка! Нет аргументов!");
+		else {
+			
+        String destString = args[args.length - 1];
+
+        File destFile = new File(destString);
+        if (!destFile.isDirectory() && args.length > 2)
+        {
+            System.out.println("Неверные аргументы! Последний аргумент должен быть директорией!");
+            return;
+        }
+
+        try {
+            if (destFile.isDirectory()) {
+                for (int i = 0; i < args.length - 1; i++) {
+                    File curFile = new File(args[i]);
+                    curFile.renameTo(new File(destFile, curFile.getName()));
+                }
+            } else {
+                {
+                    File curFile = new File(args[0]);
+                    boolean a = curFile.renameTo(destFile);
+                    a=true;
+                }
+            }
+        } catch (IOError a)
+        {
+            System.out.println("Неверный аргумент!");
+        }
+		}
+    
+    }
+}
---
+++ lab1/src/test/java/ru/spbstu/telematics/java/MVTests.java
@@ -0,0 +1,47 @@
+package ru.spbstu.telematics.java;
+
+
+
+
+import org.junit.Test;
+import java.io.File;
+import java.io.IOException;
+
+
+public class MVTests {
+
+	@Test
+    public void Test1() throws IOException {
+        File f1 = new File("testFile.txt");
+        f1.createNewFile();
+        MV.main(new String[] {f1.getPath(), "renamedTestFile.txt"});
+        f1.delete();
+
+        File f2 = new File("renamedTestFile.txt");
+
+        if(f2.exists())
+            System.out.println("File renamed. Test 1 passed.");
+        else
+            System.out.println("File don't renamed. Test 1 failed");
+
+        f2.delete();
+    }
+
+    @Test
+    public void Test2() throws IOException {
+        File f1 = new File("testFile.txt");
+        f1.createNewFile();
+        MV.main(new String[] {f1.getPath(), "..\\"});
+        f1.delete();
+
+        File f2 = new File("..\\testFile.txt");
+
+        if(f2.exists())
+            System.out.println("File removed. Test 2 passed.");
+        else
+            System.out.println("File don't removed. Test 2 failed");
+
+        f2.delete();
+    }
+
+}
